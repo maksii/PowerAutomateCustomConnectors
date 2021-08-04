5b4bdde0556829e8cfbfe5799d9afa646a7adf37
Maks Martynov <1761348+maksii@users.noreply.github.com>
2021-08-04 22:44:18 +0300

    Custom Code template solution for debug

---
+++ PowerAutomate.CustomCode/PowerAutomate.CustomCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PowerAutomate.CustomCode.Helpers;
+
+namespace PowerAutomate.CustomCode
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			var script = new Script();
+			var scriptResult = script.ExecuteAsync();
+		}
+	}
+}
